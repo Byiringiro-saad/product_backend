29358aed3a0ffeeb4a5fc408aba20914f185c74b
Saad <byiringirosaad@gmail.com>
2023-12-24 08:30:00 +0200

    login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcryptjs");
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
+// load env variables
+require("dotenv").config();
 
 // database
 const db = require("../services/db.service");
@@ -35,7 +39,43 @@ router.post("/signup", (req, res) => {
 
 //login
 router.post("/login", (req, res) => {
-  res.send("login");
+  const data = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  try {
+    // check if user exists
+    const query = `SELECT * FROM users WHERE email = ?`;
+    db.query(query, [data.email], async (err, result) => {
+      if (err) throw err;
+
+      if (result.length === 0) {
+        res.status(401).json({ message: "Invalid credentials" });
+      } else {
+        // compare password
+        bcrypt.compare(data.password, result[0].password, (err, isMatch) => {
+          if (err) throw err;
+
+          if (isMatch) {
+            // create token
+            const token = jwt.sign(
+              { id: result[0].id, email: result[0].email },
+              process.env.SECRET,
+              { expiresIn: "1h" }
+            );
+
+            // return response
+            res.status(200).json({ token });
+          } else {
+            res.status(401).json({ message: "Invalid credentials" });
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 module.exports = router;
